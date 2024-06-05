552f02f824d3dff2a8e7a537770c45ecfaba1957
Aqib1422 <mraqibshafique@gmail.com>
2024-06-06 01:03:03 +0500

    minidatabase

--- src/App.js
+++ src/App.js
@@ -1,22 +1,7 @@
 import React from 'react'
 import './style.css'
 import BookDetails from './BookDetails'
-const minidatabase = [
-  {
-    id: 1,
-    Image:"https://m.media-amazon.com/images/I/71jq9dBSp3L._AC_SY450_.jpg",
-    Heading: 'Book1',
-    Description: 'This Is book1',
-    price:2000
-  },
-  {
-    id: 2,
-    Image:"https://m.media-amazon.com/images/I/71jq9dBSp3L._AC_SY450_.jpg",
-    Heading: 'Book2',
-    Description: 'This Is book2',
-    price:3000
-  }
-]
+import { minidatabase } from './Minidatabase'
 const App = () => {
   return (
     <div className='p-3 Container holder gap-2'>
---
+++ src/Minidatabase.js
@@ -0,0 +1,23 @@
+export const minidatabase = [
+  {
+    id: 1,
+    Image:"https://m.media-amazon.com/images/I/71jq9dBSp3L._AC_SY450_.jpg",
+    Heading: 'Book1',
+    Description: 'This Is book1',
+    price:2000
+  },
+  {
+    id: 2,
+    Image:"https://m.media-amazon.com/images/I/71jq9dBSp3L._AC_SY450_.jpg",
+    Heading: 'Book2',
+    Description: 'This Is book2',
+    price:3000
+  },
+  {
+    id: 3,
+    Image:"https://m.media-amazon.com/images/I/71jq9dBSp3L._AC_SY450_.jpg",
+    Heading: 'Book2',
+    Description: 'This Is book2',
+    price:3000
+  }
+]
